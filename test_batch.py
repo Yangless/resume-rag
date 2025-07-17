64c8d288c28f6f876b596efe71e2f91a040698ca
Yangless <bielangmeng@163.com>
2025-07-17 13:26:34 +0800

    add Qwen3

--- test_batch.py
+++ test_batch.py
@@ -3,10 +3,13 @@ import os
 import time
 import asyncio
 import re
+from typing import Optional
+
 import json5
 import pandas as pd
 from fastapi import FastAPI, Header, HTTPException,Depends
 from pydantic import BaseModel
+from transformers import AutoTokenizer
 from vllm import LLM, SamplingParams
 
 token_init=os.getenv('TOKEN','d21b5ecb0ade56ca789a959e2bb57074')
@@ -33,10 +36,22 @@ def verify_token(token:str=Header(...)):
         logger.error(f'token错误:{e}')
 
 class Config:
-    model_path = "qwen2.5-1.5B"
-    output_dir = "qwen2.5-resume-finetune1"
+    temperature: Optional[float] = 1.0
+    top_p: Optional[float] = 1.0
+    max_tokens: int
+    presence_penalty: Optional[float] = 0.0
+    min_tokens: Optional[int] = 400
+    instruct: bool
+    segment: bool
+    repetition_penalty: Optional[float] = 1.0
+    top_k: Optional[int] = 0
+    max_num_seqs:Optional[int] = 10
+    gpu_memory_utilization:Optional[float] = 0.9
+    model_path: str=None
+    alter: Optional[bool] = False
 
-config = Config()
+
+config=Config()
 
 MAX_CONCURRENT_REQUESTS = 2
 semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
@@ -45,20 +60,24 @@ semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
 #executor = ProcessPoolExecutor(max_workers=NUM_MODEL_COPIES)
 
 
-sampling_params = SamplingParams(
-    temperature=0.0,
-    max_tokens=600,
-    #stop=["```"]  # 如果输出包含 ```json ... ```，可以提前停止
-)
-
 llm = None
 
-def split_and_merge_chunks(chunks, max_length=1024):
+def split_and_merge_chunks(chunks, max_length=4000):
     try:
         merged_chunks = []
         current_chunk = ""
         for i,chunk in enumerate(chunks):
-            if len(current_chunk) + len(chunk) + 1 > max_length:
+            if len(chunk)>max_length:
+                t0=0
+                for t in range(max_length,len(chunk),max_length):
+                    if t0==0:
+                        merged_chunks.append(chunk[:t])
+                    else:
+                        merged_chunks.append(chunk[t0:t])
+                    t0 = t - 100
+                continue
+
+            if len(current_chunk) + len(chunk)  > max_length:
                 merged_chunks.append(current_chunk)
                 current_chunk = '\n'.join(chunks[i-3:i]) + chunk + "\n"
             else:
@@ -68,36 +87,42 @@ def split_and_merge_chunks(chunks, max_length=1024):
         if len(current_chunk)>200:
             merged_chunks.append(current_chunk)
         else:
-            test_200='\n'.join(chunks[-15:])
-            merged_chunks.append(test_200)
-        if not merged_chunks:
-            merged_chunks.append(current_chunk)
+            if len(chunks[-6:])>50:
+                test_200='\n'.join(chunks[-6:])
+                merged_chunks.append(test_200)
+        #if not merged_chunks:
+            #merged_chunks.append(current_chunk)
+        #print(merged_chunks)
         return merged_chunks
     except Exception as e:
         logger.error(f"模型拆分出错:{e}")
         return []
 
-def load_vllm_model():
+def load_vllm_model(config):
     global llm
-    if llm is None:
-        llm = LLM(model="qwen2.5-1.5B-merge1", trust_remote_code=True,
-                  dtype="half", tensor_parallel_size=1)
+    if llm is None or config.alter:
+        llm = LLM(model=config.model_path, trust_remote_code=True,dtype="bfloat16",
+                   tensor_parallel_size=1,
+                  max_num_seqs=config.max_num_seqs,
+                  gpu_memory_utilization=config.gpu_memory_utilization,
+                  )# dtype="half",
     return llm
 
 
 def extract_first_json(text):
-    pattern = r'{.*?}'
+    pattern = r'\{.*?\}'
     try:
         match = re.findall(pattern, text, flags=re.DOTALL)[-1]
         return match
     except Exception as e:
-        #logger.error(f'提取JSON1出错:{e}')
-        pattern = r'{(.*)'
+        logger.info(f'提取JSON1出错:{e}')
+        text_='{'+text
+        pattern = r'(\{.*)```'
         try:
-            match = re.findall(pattern, text, flags=re.DOTALL)[-1]
+            match = re.findall(pattern, text_, flags=re.DOTALL)[-1]
             return match
         except Exception as e:
-            logger.info(f'提取JSON2出错:{e}', text)
+            logger.info(f'提取JSON2出错:{e}:{text}')
             return str(text)
 
 def process_json(text):
@@ -116,15 +141,16 @@ def process_json(text):
             index = int(text_index[key] + len(key))
             if i < len(text_index_keys) - 1:
                 next_key = text_index_keys[i + 1]
-                text_dict[key] = text[index:text_index[next_key]]
+                text_dict[key] = text[index:text_index[next_key]].replace(':','')
             else:
-                text_dict[key] = text[index:]
+                text_dict[key] = text[index:].replace(':','')
         if not text_dict:
             return text
-        return text_dict
+        #logger.info(f'JSON数据：{str(text_dict)}')
+        return str(text_dict)
     except Exception as e:
         print(e)
-        return text
+        return text.replace(':','')
 
 
 def diversity_ratio(s):
@@ -132,31 +158,48 @@ def diversity_ratio(s):
     total_chars = len(s)
     return unique_chars / total_chars
 
-async def run_inference_on_gpu(resumes):
+async def run_inference_on_gpu(resumes,sampling_params,config):
     try:
-        llm = load_vllm_model()
+        llm = load_vllm_model(config)
 
-        input_texts = []
-        for resume in resumes:
-            input_text = f"""从下面这段简历文本中提取涉及以下7类标签的总结：基本信息,教育经历,工作经历,项目经历,培训经历,个人评价,技能证书。
-            每份工作或项目经历字数不超过100字;如果标签没有提取到内容则返回空字符串;不要嵌套JSON。
+        if not config.instruct:
+            input_texts = []
+            for resume in resumes:
+                input_text = f"""从下面这段简历文本中提取涉及以下7类标签的总结：基本信息,教育经历,工作经历,项目经历,培训经历,个人评价,技能证书。
+                                每份工作或项目经历字数不超过100字;如果标签没有提取到内容则返回空字符串;不要嵌套JSON。
 
-    简历文本:
-    {resume}
+                        简历文本:
+                        {resume}
 
-    返回JSON格式:
-    ```json
-    {{"基本信息":"","教育经历":[],"工作经历":[],"项目经历":[],"培训经历":[],"个人评价":"","技能证书":""}}
-    ```
-    """
-            input_texts.append(input_text)
+                        返回JSON格式:
+                        ```json
+                        {{"基本信息":"","教育经历":[],"工作经历":[],"项目经历":[],"培训经历":[],"个人评价":"","技能证书":""}}
+                        ```
+                        """
+                input_texts.append(input_text)
+            print(f"正在批量处理 {len(input_texts)} 条简历...")
+            outputs = llm.generate(input_texts, sampling_params)
 
-        print(f"正在批量处理 {len(input_texts)} 条简历...")
+        else:
+            tokenizer = AutoTokenizer.from_pretrained(config.model_path)
 
-        outputs = llm.generate(input_texts, sampling_params)
+            batched_inputs = [
+                tokenizer.apply_chat_template(
+                    [{"role": "user", "content": prompt}],
+                    tokenize=False,
+                    add_generation_prompt=True,
+                    enable_thinking=True,
+                )
+                for prompt in resumes
+            ]
+
+            outputs = llm.generate(batched_inputs, sampling_params)
 
         responses = [output.outputs[0].text for output in outputs]
+
+        #logger.info(responses)
         cleaned_responses = [extract_first_json(r) for r in responses]
+
         return cleaned_responses
     except Exception as e:
         logger.error(f"推理错误: {e}")
@@ -165,13 +208,26 @@ async def run_inference_on_gpu(resumes):
 
 class TextRequest(BaseModel):
     text:dict
+    temperature:Optional[float]=1.0
+    top_p:Optional[float]=1.0
+    max_tokens:int
+    presence_penalty:Optional[float]=0.0
+    min_tokens:Optional[int]=400
+    instruct:bool
+    segment:bool
+    model_path:str
+    repetition_penalty:Optional[float]=1.0
+    top_k:Optional[int]=0
+    max_num_seqs: Optional[int] = 10
+    gpu_memory_utilization: Optional[float] = 0.9
+    alter:Optional[bool]=False
 
 app = FastAPI()
 
 
-async def async_inference(input_texts):
+async def async_inference(input_texts,sampling_params,config):
     #loop = asyncio.get_event_loop()
-    result = await run_inference_on_gpu(input_texts)
+    result = await run_inference_on_gpu(input_texts,sampling_params,config)
     return result
 def merge_result(text):
     info, edu, work, project, train, evalu, skill = [], [], [], [], [], [], []
@@ -182,47 +238,49 @@ def merge_result(text):
         try:
             if not text_i:
                 continue
-            if diversity_ratio(text_i)<0.1:
-                continue
+            #if diversity_ratio(text_i)<0.1:
+                #continue
 
             #print(text_i)
             text_i = re.sub(r'[\n\s]+', ' ', text_i)
             text_i=re.sub('，',',',text_i)
             try:
                 text_dict = json5.loads(text_i)
-            except Exception as e:
+            except Exception as e1:
                 #else_info.append(str(text_i))
                 #continue
-                text_i=re.sub(r'[][{}"]+', '', str(text_i))
-                text_i = re.sub(r'：', ':', text_i)
-                text_i_=process_json(text_i)
-                text_i_ = re.sub(r":,", "''", text_i_)
                 try:
+                    text_i = re.sub(r'\[\\]', '\"\"', str(text_i))
+                    text_i=re.sub(r'[][{}"]+', '', str(text_i))
+                    text_i = re.sub(r'：', ':', text_i)
+                    text_i_=process_json(text_i)
+                    #text_i_ = re.sub(r":+\s*,", ":''", text_i_)
                     text_dict = json5.loads(text_i_)
-                except Exception as e:
-                    #print(text_i_)
-                    print('重制JSON失败',e)
+                except Exception as e2:
+                    print('重制JSON失败',e2)
                     else_info.append(str(text_i))
                     continue
 
-            def safe_add(lst, val):
-                if isinstance(val, (str, int, float)):
-                    lst.append(str(val))
-                elif isinstance(val, list):
-                    lst.extend([str(v) for v in val if v is not None])
-                elif val is not None:
-                    lst.append(str(val))
+            try:
+                def safe_add(lst, val):
+                    if val is not None:
+                        lst.append(str(val))
+                    elif isinstance(val, (str, int, float)):
+                        lst.append(str(val))
+                    elif isinstance(val, list):
+                        lst.extend([str(v) for v in val if v is not None])
+                safe_add(info, text_dict.get("基本信息",None))
+                safe_add(edu, text_dict.get("教育经历",None))
+                safe_add(work, text_dict.get("工作经历",None))
+                safe_add(project, text_dict.get("项目经历",None))
+                safe_add(train, text_dict.get("培训经历",None))
+                safe_add(evalu, text_dict.get("个人评价",None))
+                safe_add(skill, text_dict.get("技能证书",None))
+            except Exception as e4:
+                print(e4)
 
-            safe_add(info, text_dict.get("基本信息"))
-            safe_add(edu, text_dict.get("教育经历"))
-            safe_add(work, text_dict.get("工作经历"))
-            safe_add(project, text_dict.get("项目经历"))
-            safe_add(train, text_dict.get("培训经历"))
-            safe_add(evalu, text_dict.get("个人评价"))
-            safe_add(skill, text_dict.get("技能证书"))
-
-        except Exception as e:
-            logger.info(f"转换为JSON出错:{e}:{text}")
+        except Exception as e3:
+            logger.info(f"转换为JSON出错:{e3}:{text_i}")
 
     for name,resume_list in zip(["基本信息","教育经历","工作经历","项目经历","培训经历","个人评价","技能证书"],[info,edu,work,project,train,evalu,skill]):
         resume_list_=list(filter(None,resume_list))
@@ -230,38 +288,85 @@ def merge_result(text):
             merged[name]=resume_list_
     if else_info:
         merged['其他信息']=else_info
-    return re.sub(r'[][{}\'"]+', '', str(merged))
+    merged=re.sub(r'[][{}\'"]+', '', str(merged))
+    merged=re.sub(r'[,，\s]+',',',merged)
+    return merged
 
 
 @app.post("/summary")
 async def analyze_text(data: TextRequest,token:str=Depends(verify_token)):
     try:
+        config.temperature=data.temperature
+        config.top_p=data.top_p
+        config.max_tokens=data.max_tokens
+        config.presence_penalty=data.presence_penalty
+        config.min_tokens=data.min_tokens
+        config.repetition_penalty=data.repetition_penalty
+        config.instruct=data.instruct
+        config.segment=data.segment
+        config.top_k=data.top_k
+        config.model_path = data.model_path
+        #logger.info(model_path)
+        ###
         data=data.text
         start = time.time()
         text_id=[]
         text=[]
         data_text = list(map(lambda x: x.replace('\\r', '\r').replace('：', ':').replace('\\n', '\n'), data.values()))
-        for i,i_text in zip(data.keys(),data_text):
-            i_text_list=i_text.splitlines()
-            list_text=split_and_merge_chunks(i_text_list)
-            text_id.extend([i]*len(list_text))
-            text.extend(list_text)
-        result = await async_inference(text)
-        #print(result)
+        if config.segment:
+            for i,i_text in zip(data.keys(),data_text):
+                #logger.info('s')
+                i_text_list=i_text.splitlines()
+                #logger.info(f'{i_text_list}')
+                list_text=split_and_merge_chunks(i_text_list)
+                list_text=list(filter(bool, list_text))
+                #logger.info(f'原文:{list_text}')
+                text_id.extend([i]*len(list_text))
+                text.extend(list_text)
+            config.max_tokens=int(0.45*max(len(i) for i in text))
+            sampling_params = SamplingParams(
+                temperature=config.temperature,
+                top_p=config.top_p,
+                max_tokens=config.max_tokens,
+                presence_penalty=config.presence_penalty,
+                min_tokens=config.min_tokens,
+                repetition_penalty=config.repetition_penalty,
+                top_k=config.top_k
 
-        df=pd.DataFrame(zip(text_id,result),columns=['id','text'])
-        #print(df)
-        df_merge = (
-            df.groupby('id', as_index=False,sort=False)['text']
-            .apply(lambda x: merge_result(x))
-            .reset_index(drop=True)
-        )
-        df_merge.columns = ['id', 'merged_info']
-        #print(df_merge)
-        result_end=df_merge.set_index('id')['merged_info'].to_dict()
-        end = time.time()
-        logger.info(f"耗时：{end - start}" )
-        return result_end
+            )
+            result = await async_inference(text,sampling_params,config)
+            #logger.info(f'模型输出:{result}')
+
+            df=pd.DataFrame(zip(text_id,result),columns=['id','text'])
+            #print(df)
+            df_merge = (
+                df.groupby('id', as_index=False,sort=False)['text']
+                .apply(lambda x: merge_result(x))
+                .reset_index(drop=True)
+            )
+            df_merge.columns = ['id', 'merged_info']
+            #print(df_merge)
+            result_end=df_merge.set_index('id')['merged_info'].to_dict()
+            end = time.time()
+            logger.info(f"耗时：{end - start}" )
+            return result_end
+        else:
+            sampling_params = SamplingParams(
+                temperature=config.temperature,
+                top_p=config.top_p,
+                max_tokens=config.max_tokens,
+                presence_penalty=config.presence_penalty,
+                min_tokens=config.min_tokens,
+                repetition_penalty=config.repetition_penalty,
+                top_k=config.top_k
+
+            )
+            result = await async_inference(data_text, sampling_params,config)
+            result_end={key:value for key,value in zip(data.keys(),result)}
+            end = time.time()
+            logger.info(f"耗时：{end - start}")
+            return result_end
+
     except Exception as e:
         logger.error(f'模型推理出错: {e}')
         return {"error": str(e)}
@@ -269,7 +374,6 @@ async def analyze_text(data: TextRequest,token:str=Depends(verify_token)):
 
 if __name__ == '__main__':
     import uvicorn
-
     uvicorn.run(
         app="test_batch:app",
         host="0.0.0.0",
